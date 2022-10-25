abed91ead2eec7f74bf47d23905e61f70207854e
Anh Le <lhanhfire1999@gmail.com>
2022-10-26 01:04:40 +0700

    Feat(Sub-project): Use shared-module and handle async script loading

--- cart/src/index.js
+++ cart/src/index.js
@@ -1,5 +1,1 @@
-import faker from 'faker';
-
-let cartText = `You have ${faker.random.number()} items in your cart`;
-
-document.querySelector('#dev-cart').innerHTML = cartText;
+import('./container');
