5ab588f08940e5b71e9a9c961b6f2aecf079d388
Anh Le <anh.lee@digicommercegroup.com>
2022-10-29 20:31:16 +0700

    Feat(Container & Marketing): Intergration of the Container and Marketing

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,5 +1,7 @@
 const { merge } = require('webpack-merge')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin')
+
 const commonConfig = require('./webpack.common')
 
 const devConfig = {
@@ -13,6 +15,12 @@ const devConfig = {
   plugins: [
     new HtmlWebpackPlugin({
       template: './public/index.html',
+    }),
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
     }),
   ],
 }
--- container/src/App.js
+++ container/src/App.js
@@ -1,5 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
+import { mount as MarketingMount } from 'marketing/MarketingApp'
+
+console.log(MarketingMount)
 
 const root = createRoot(document.querySelector('#root'))
 root.render(<h1>Container</h1>)
